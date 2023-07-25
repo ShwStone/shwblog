38924a8f2fc76fc3b4845fbdd8acc4f513317928
ShwStone <HaowenShi@outlook.com>
2023-07-25 18:34:28 +0800

    0725

--- build.sh
+++ build.sh
@@ -3,8 +3,6 @@
 yarn global add hexo-cli
 yarn
 
-cp copy/spoiler.css node_modules/hexo-sliding-spoiler/assets/spoiler.css -f
-cp copy/renderer.js node_modules/hexo-renderer-kramed/lib/renderer.js -f
-cp copy/inline.js node_modules/kramed/lib/rules/inline.js -f
+cp copy/spoiler.css node_modules/hexo-sliding-spoiler/assets/spoiler.css
 
 yarn build
